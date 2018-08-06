e9bf944aea879010360195be48e0f2b7811e791d
Siqueira <thiago.siqueira@accenture.com>
2018-08-06 16:24:56 -0300

    'Adicionada_dependencia_Jackson'

--- src/main/java/com/thiago/utils/services/ServicesValidator.java
+++ src/main/java/com/thiago/utils/services/ServicesValidator.java
@@ -5,7 +5,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-import com.thiago.utils.models.ReturnObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thiago.utils.validadores.ValidadorCNPJ;
 import com.thiago.utils.validadores.ValidadorCPF;
 import com.thiago.utils.validadores.interfaces.IValidadorCNPJ;
@@ -16,19 +17,28 @@ public class ServicesValidator {
 	
 	private IValidadorCPF validadorCPF = new ValidadorCPF();
 	private IValidadorCNPJ validadorCNPJ = new ValidadorCNPJ();
+	private ObjectMapper mapper = new ObjectMapper();
 	
 	@GET
 	@Path("cpf/{cpf}")
-	@Produces("application/json")
-	public ReturnObject validaCPF(@PathParam("cpf") String cpf) {
-		return validadorCPF.validaCPF(cpf);
+	@Produces({"application/xml", "application/json"})
+	public String validaCPF(@PathParam("cpf") String cpf){
+		try {
+			return mapper.writeValueAsString(validadorCPF.validaCPF(cpf));
+		}catch(JsonProcessingException e) {
+			return "Erro ao validar CPF::"+e.getMessage();
+		}	
 	}
 	
 	@GET
 	@Path("cnpj/{cnpj : .+}")
-	@Produces("application/json")
-	public ReturnObject validaCNPJ(@PathParam("cnpj") String cnpj) {
-		return validadorCNPJ.validaCNPJ(cnpj);
+	@Produces({"application/xml", "application/json"})
+	public String validaCNPJ(@PathParam("cnpj") String cnpj){
+		try {
+			return mapper.writeValueAsString(validadorCNPJ.validaCNPJ(cnpj));
+		}catch(JsonProcessingException e) {
+			return "Erro ao validar CNPJ::"+e.getMessage();
+		}		
 	}
 
 }
